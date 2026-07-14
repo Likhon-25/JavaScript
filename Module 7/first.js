1f293a9a7e639f1d5a568ae33f8177962c34a5f3
Likhon-25 <likhonmondol327@gmail.com>
2026-07-15 00:48:18 +0600

    Learn Veriable

---
+++ Module 7/first.js
@@ -0,0 +1,2 @@
+console.log("Bishmillah")
+console.log("Assalamulaikum")
---
+++ Module 7/veriable.js
@@ -0,0 +1,12 @@
+// var
+var name = "Milon";
+var age = 45;
+var temp = 35
+console.log(age);
+console.log(temp);
+
+// const & let
+const MyName = "Likhon Mondol";
+let MyAge = 20;
+console.log("My Name is :", MyName);
+console.log("I;m " ,MyAge ,"Years Old");
