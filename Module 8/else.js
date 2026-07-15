07ebd085b7896fda97bcaae0362df38792c0812c
Likhon-25 <likhonmondol327@gmail.com>
2026-07-15 22:42:27 +0600

    learn if and else conditin

---
+++ Module 8/else.js
@@ -0,0 +1,8 @@
+const rickshaVara = 100;
+
+if(rickshaVara < 50){
+    console.log("Mama hood ta tula dan");
+    console.log("Go slowly");
+} else{
+    console.log("Amr kasa tk nai. ame hata bari jbo");
+}
--- Module 8/if.js
+++ Module 8/if.js
@@ -4,3 +4,16 @@ if(8 > 3){
 if(8 < 3){
     console.log("Yes It's False");
 }
+
+if(50 <= 45){
+    console.log("No 50 not grater den or equal 45");
+}
+if(50 <= 50){
+    console.log("Commint inside the condition");
+}
+
+
+const price = 400;
+if(price < 500){
+    console.log("Price is lower");
+}
