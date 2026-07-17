dc63922081ebdbd25c6087fe79f4ffa050103fbc
Likhon-25 <likhonmondol327@gmail.com>
2026-07-18 03:29:40 +0600

    pracice problem solving

---
+++ Module 9/problem-practice.js
@@ -0,0 +1,31 @@
+// Task 1 — Print "Hello" Five Times
+// Write a for loop that prints "Hello" 5 times.
+// Your code here
+
+for(i = 0; i < 5; i++){
+    // console.log("Hello" , i);
+}
+
+// -------------------------------------------------
+
+// Task 2 — Print Numbers 1 to 10 
+// Write a for loop that prints numbers from 1 to 10.
+// // Your code here
+ 
+for(a = 1; a <= 10; a++){
+    // console.log('i value : ', a);
+} 
+
+// --------------------------------------------------
+
+// Task 3 — Countdown with While Loop 
+// Write a while loop that prints numbers from 10 down to 1.
+// // Your code here
+
+let b = 10;
+while(b > 1){
+   b--;
+   console.log(b);
+}
+ 
+
