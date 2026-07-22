e69c551c819078821e594e12e018e4f913de6e2e
Likhon-25 <likhonmondol327@gmail.com>
2026-07-22 11:31:10 +0600

    strig

---
+++ Module 11 String/string.js
@@ -0,0 +1,15 @@
+let name = "Likhon";
+let country = "Bangladesh";
+let message = "Hello Wordl";
+
+// console.log(name);
+// console.log(country);
+// console.log(message);
+
+let text = "Bangladesh";
+
+console.log(text[0]);
+console.log(text[3]);
+console.log(text[6]);
+
+console.log(text.length);
