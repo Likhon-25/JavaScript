22b0fb1ba563d5ef20672b4a75bfe0843dc81058
Likhon-25 <likhonmondol327@gmail.com>
2026-07-22 18:59:06 +0600

    learn object

---
+++ Module 11 String/object.js
@@ -0,0 +1,50 @@
+// let student = {
+//     name: "Likhon",
+//     age: 18,
+//     department: "CST",
+//     cgpa: 3.80
+// };
+// console.log(student);
+
+
+
+let person = {
+    name: 'Likhon'
+}
+
+let key = 'name';
+// console.log(person[key]);
+// console.log(person.name);
+
+
+
+let introduct = {
+    name: 'Likhon',
+    age:  18
+}
+
+introduct.age = 20;
+introduct.country = 'Bangladesh'
+// console.log(introduct);
+delete introduct.age;
+// console.log(introduct);
+
+
+
+
+let student = {
+    name: "Likhon",
+    department: "CST",
+    semester: 6
+};
+let value = student['department']
+console.log(value);
+// console.log(student.department);
+
+
+let car = {
+    name: 'BMW'
+}
+car.color = 'Black'
+console.log(car);
+
---
+++ Module 11 String/split.js
@@ -0,0 +1,5 @@
+const company = "tesla , spaceX, Payple, Walton, Twiter";
+const companies = company.split(" ");
+console.log(companies);
+const againSte = companies.join(" ");
+console.log(againSte);
