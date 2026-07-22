ca2ff5c069c4027ea673baa9ce3dac081b48e679
Likhon-25 <likhonmondol327@gmail.com>
2026-07-23 01:21:21 +0600

    start js Function

---
+++ Module 12 Function/syntax.js
@@ -0,0 +1,49 @@
+// function functionName() {}
+
+
+// function add(a, b) {
+  //   console.log(a + b);
+// }
+// add(1, 2);
+// add(20, 10);
+
+
+
+function welcome() {
+//   console.log("Welcome ");
+}
+welcome();
+welcome();
+welcome();
+welcome();
+welcome();
+
+function greet(name) {
+//   console.log("Welcome", name);
+}
+greet("Likhon");
+greet("Misba");
+greet("Shohag");
+
+
+function result (a, b){
+    return a+b
+}
+
+let add = result(20, 30)
+console.log(add);
+
+// -------------------------
+
+function subs (a , b){
+    return a - b
+}
+let minas = subs( 20, 5)
+// console.log(minas);
+
+
+function summation( p , q){
+    return p + q;
+}
+let totalSum = summation(30, 35)
+// console.log(totalSum);
