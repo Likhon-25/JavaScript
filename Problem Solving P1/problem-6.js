48f964f6e481197359c888ab1f9f976be0742fcc
Likhon-25 <likhonmondol327@gmail.com>
2026-07-26 01:27:09 +0600

    solved 6th problem

---
+++ Problem Solving P1/problem-6.js
@@ -0,0 +1,36 @@
+// Problem 6: Count Vowel in  a string
+
+// Without functin
+// let str = "Programming Herooo";
+// let Vowel =  'a,e,i,o,u'
+
+// let count = 0;
+
+// for(let i = 0; i < str.length; i++){
+//     let letter = str[i];
+//     console.log("i => letter =>>>", i , letter);
+//     if(Vowel.includes(letter)){
+//         console.log("Vowel");
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// With function
+function countVowel(str) {
+  let Vowel = "a,e,i,o,u";
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    console.log("i => letter =>>>", i, letter);
+    if (Vowel.includes(letter)) {
+      console.log("Vowel");
+      count++;
+    }
+  }
+  return count;
+}
+
+let findVowel = countVowel("Programming Herooooo");
+console.log(findVowel);
