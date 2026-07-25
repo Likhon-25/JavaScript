551f7822144fa7963248a37afed346edd469bc61
Likhon-25 <likhonmondol327@gmail.com>
2026-07-25 18:39:28 +0600

    solved problem 1

---
+++ Problem Solving P1/problem-1.js
@@ -0,0 +1,11 @@
+// problem -1 :  Check if a Number is Even or Odd 
+
+function checkEvenOdd(Number){
+    if(Number%2=== 0){
+        return "Event Number"
+    } else{
+        return "Odd Number"
+    }
+}
+console.log(checkEvenOdd(34));
+console.log(checkEvenOdd(33));
