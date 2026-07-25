de2c332adef725a33058e3004803f92ae9d0426c
Likhon-25 <likhonmondol327@gmail.com>
2026-07-26 03:16:41 +0600

    Problem 8th Solved

---
+++ Problem Solving P1/problem-8.js
@@ -0,0 +1,22 @@
+// Problem 8 : Coumt word in  a sentence
+
+// let arr = ["Likhon", "Mondol", "Engineer"]
+// console.log(arr.length);
+
+// Without Functiohn
+// let str = "Likhon Mondol";
+// let length = str.split(" ");
+// console.log(length.length);
+
+// console.log(str.split(" "));
+
+
+
+// With Function
+function wordCount(str){
+    let splitWord = str.split(" ");
+    return splitWord.length;
+}
+let word = wordCount("Md Likhon Mondol")
+console.log(word);
+console.log(wordCount("Hello Bangladesh I Love your"));
