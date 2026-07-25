9e4d019251b77184e9b0e155acc5bb7de6a88203
Likhon-25 <likhonmondol327@gmail.com>
2026-07-25 13:57:56 +0600

    learn function

--- Module 12 Function/different.js
+++ Module 12 Function/different.js
@@ -12,12 +12,41 @@ const myPrice = doubleOrHalf(100, true);
 const leaderPrice = doubleOrHalf(100, false);
 // console.log("Price for Leader",leaderPrice);
 
+// function firstElementDouble(array) {
+//   console.log("Got the Parameter value", array);
+//   const firstElement = array[0];
+//   const doubled = firstElement * 2;
+//   return doubled;
+// }
+// const number = [11, 34, 5, 78, 99, 87, 85, 67, 43];
+// const doubled = firstElementDouble(number);
+// console.log(doubled);
+
 function firstElementDouble(array) {
-  console.log("Got the Parameter value", array);
+  console.log("Get the parameter value", array);
   const firstElement = array[0];
   const doubled = firstElement * 2;
-  return doubled;
+  return doubled
 }
-const number = [11, 34, 5, 78, 99, 87, 85, 67, 43];
+
+const number = [4,2, 2, 3, 4, 5, 6];
 const doubled = firstElementDouble(number);
-console.log(doubled);
+// console.log(doubled);
+
+
+const student = {
+  name: 'Likhon',
+  age: 20,
+  marks: 85
+}
+function isSuudentAPlus (student){
+  console.log('got the student', student);
+  const marks = student.marks;
+  console.log(marks);
+  if(marks >= 80){
+    return true
+  } else{
+    false
+  }
+}
+const isAPlus = isSuudentAPlus(student)
---
+++ Module 12 Function/multiple.js
@@ -0,0 +1,12 @@
+function multiplyTwoNumber(a, b) {
+  const mul = a * b;
+//   console.log(mul);
+return mul
+}
+
+let val1 = multiplyTwoNumber(3, 5);
+let val2 = multiplyTwoNumber(5, 7);
+
+
+let mulOfTwo = `multiply of two number is ${val1} and ${val2}`
+console.log(mulOfTwo);
