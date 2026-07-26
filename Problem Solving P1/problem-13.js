9b5e8d71954e57097395cce91fd8f171dfc765d4
Likhon-25 <likhonmondol327@gmail.com>
2026-07-26 16:29:26 +0600

    problem 13 solved

---
+++ Problem Solving P1/problem-13.js
@@ -0,0 +1,33 @@
+// Problem 13: Loop thorugh and Object's Propertieis
+
+let monitor ={
+    color: "Black",
+    brand: "Hp",
+    display: 24,
+    size: "Something",
+    test: "Something"
+}
+console.log(monitor["color"]); //braket notation
+console.log(monitor.color); //dot notation
+
+// for loop
+for(let key in monitor){
+    console.log(key, ": " , monitor[key]);
+}
+
+// Using keys method
+let objKey = Object.keys(monitor);
+console.log(objKey);
+
+// for(let i = 0; i < objKey.length; i++){
+//     let key = objKey[i];
+//     console.log(key , ": ", monitor[key]);
+// }
+
+
+
+for(let key of objKey){
+    console.log(key , ": ", monitor[key]);
+}
+
+
